354504bcd90665689a69299fa22a547c0cf53f80
Sangshetty Patil <sangshetty.patil@xilinx.com>
2023-04-03 13:31:27 -0700

    partial.pdi path fix

--- apu_app/power_demo.sh
+++ apu_app/power_demo.sh
@@ -6,52 +6,53 @@ DelayVal=30
 IterationCnt=1
 
 if [ -d "/sys/firmware/devicetree/base/firmware/zynqmp-firmware" ]; then
-        Interface="zynqmp"
-        echo "APU: ZynqMP interface"
-        PGGS_INTERFACE=/sys/devices/platform/firmware:zynqmp-firmware/pggs3
+    Interface="zynqmp"
+    echo "APU: ZynqMP interface"
+    PGGS_INTERFACE=/sys/devices/platform/firmware:zynqmp-firmware/pggs3
 
-        # Disable active wakeups
-        echo disabled > /sys/devices/platform/axi/ffa60000.rtc/power/wakeup
-	echo disabled > /sys/devices/platform/axi/ff000000.serial/power/wakeup
-	echo disabled > /sys/devices/platform/axi/ff010000.serial/power/wakeup
-	echo disabled > /sys/devices/platform/axi/ff0a0000.gpio/power/wakeup
+    # Disable active wakeups
+    echo disabled > /sys/devices/platform/axi/ffa60000.rtc/power/wakeup
+    echo disabled > /sys/devices/platform/axi/ff000000.serial/power/wakeup
+    echo disabled > /sys/devices/platform/axi/ff010000.serial/power/wakeup
+    echo disabled > /sys/devices/platform/axi/ff0a0000.gpio/power/wakeup
 else
-        Interface="versal"
-        echo "APU: Versal interface"
-        PGGS_INTERFACE=/sys/devices/platform/firmware:versal-firmware/pggs0
+    Interface="versal"
+    echo "APU: Versal interface"
+    PGGS_INTERFACE=/sys/devices/platform/firmware:versal-firmware/pggs0
 
-        # Disable active wakeups
-        echo disabled > /sys/devices/platform/axi/f12a0000.rtc/power/wakeup
-        echo disabled > /sys/devices/platform/axi/f12a0000.rtc/rtc/rtc0/alarmtimer.1.auto/power/wakeup
-        echo disabled > /sys/devices/platform/axi/f1020000.gpio/power/wakeup
+    # Disable active wakeups
+    echo disabled > /sys/devices/platform/axi/f12a0000.rtc/power/wakeup
+    echo disabled > /sys/devices/platform/axi/f12a0000.rtc/rtc/rtc0/alarmtimer.1.auto/power/wakeup
+    echo disabled > /sys/devices/platform/axi/f1020000.gpio/power/wakeup
 fi
+
 # Disable console suspend
 echo 0 > /sys/module/printk/parameters/console_suspend
 
 sync_apu_rpu() {
-        printf "APU: Delay ${DelayVal} seconds\n\n"
-        sleep ${DelayVal}
+    printf "APU: Delay ${DelayVal} seconds\n\n"
+    sleep ${DelayVal}
 
-        while :
-        do
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                ggs=$(( $ggs_val & 0xFF00 ))
+    while :
+    do
+        ggs_val=$(cat ${PGGS_INTERFACE})
+        ggs=$(( $ggs_val & 0xFF00 ))
 
-                usleep 100
+        usleep 100
 
-            if [ $ggs == $((0xAA00)) ]; then
-                echo "APU: Sending sync command"
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFF00FF))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-                break
-            fi
-        done
+        if [ $ggs == $((0xAA00)) ]; then
+            echo "APU: Sending sync command"
+            ggs_val=$(cat ${PGGS_INTERFACE})
+            pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
+            pggs_val=$(printf '%x\n' $pggs_val)
+            echo $pggs_val > ${PGGS_INTERFACE}
+            ggs_val=$(cat ${PGGS_INTERFACE})
+            pggs_val=$(($ggs_val & 0xFFFF00FF))
+            pggs_val=$(printf '%x\n' $pggs_val)
+            echo $pggs_val > ${PGGS_INTERFACE}
+            break
+        fi
+    done
 }
 
 # Get higher and lower APU frequencies
@@ -66,48 +67,43 @@ printf "APU: Setting all APU frequency to ${high_freq}\n\n"
 echo ${high_freq} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 echo ${high_freq} > /sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed
 if [ $Interface == "zynqmp" ]; then
-        echo ${high_freq} > /sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed
-        echo ${high_freq} > /sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed
+    echo ${high_freq} > /sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed
+    echo ${high_freq} > /sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed
 fi
 
 if [ $Interface != "zynqmp" ]; then
-        # load partial pdi
-        fpgautil -R
-        fpgautil -b /usr/bin/partial.pdi
+    # load partial pdi
+    fpgautil -R
+    fpgautil -b /usr/bin/partial.pdi
 fi
 
 yes > /dev/null &
 yes > /dev/null &
 if [ $Interface == "zynqmp" ]; then
-        yes > /dev/null &
-        yes > /dev/null &
+    yes > /dev/null &
+    yes > /dev/null &
 fi
-echo "APU: *********************** APU, RPU, PL and AIE in full power mode *****************"
+
+echo "APU: *********************** APU, RPU, PL in full power mode *************************"
 #printf "APU: Delay ${DelayVal} seconds\n\n"
 #sleep ${DelayVal}
 
-# AIE clock frequency
-#Get aie clock frequency xbutil advanced --aie-clock -g
-#Set aie clock frequency xbutil advanced --aie-clock -s 10M -d 
-
-#echo pm_set_requirement 0x18800000 1 2 1 > /sys/kernel/debug/zynqmp-firmware/pm
-#echo "APU: *********************** APU, RPU, PL in power and AIE in half frequency *********"
 printf "APU: Delay ${DelayVal} seconds\n\n"
 sleep ${DelayVal}
 
 if [ $Interface == "zynqmp" ]; then
-        echo "APU: Latency to Power OFF PL domain"
-        ggs_val=$(cat ${PGGS_INTERFACE})
-        pggs_val=$(($ggs_val & 0xFFFFFF00 | 0x11))
-        pggs_val=$(printf '%x\n' $pggs_val)
-        echo $pggs_val > ${PGGS_INTERFACE}
+    echo "APU: Latency to Power OFF PL domain"
+    ggs_val=$(cat ${PGGS_INTERFACE})
+    pggs_val=$(($ggs_val & 0xFFFFFF00 | 0x11))
+    pggs_val=$(printf '%x\n' $pggs_val)
+    echo $pggs_val > ${PGGS_INTERFACE}
 else
-        echo "APU: Powering off PL domain"
-        time fpgautil -R
-        time fpgautil -b /usr/bin/greybox.pdi
+    echo "APU: Powering off PL domain"
+    time fpgautil -R
+    time fpgautil -b /usr/bin/greybox.pdi
 fi
 
-echo "APU: ********************* APU and RPU full load, PL, AIE is OFF **********************"
+echo "APU: ********************* APU and RPU full load, PL OFF *****************************"
 printf "APU: Delay ${DelayVal} seconds\n\n"
 sleep ${DelayVal}
 
@@ -133,6 +129,7 @@ ggs_val=$(cat ${PGGS_INTERFACE})
 pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
 pggs_val=$(printf '%x\n' $pggs_val)
 echo $pggs_val > ${PGGS_INTERFACE}
+
 echo "APU: ********************* APU full load, RPU idle, PL is OFF ************************"
 sync_apu_rpu
 
@@ -141,13 +138,13 @@ time echo 0 > /sys/devices/system/cpu/cpu1/online
 sync_apu_rpu
 
 if [ $Interface == "zynqmp" ]; then
-        printf "\nAPU: Latency to Power OFF APU2 core\n"
-        time echo 0 > /sys/devices/system/cpu/cpu2/online
-        sync_apu_rpu
+    printf "\nAPU: Latency to Power OFF APU2 core\n"
+    time echo 0 > /sys/devices/system/cpu/cpu2/online
+    sync_apu_rpu
 
-        printf "\nAPU: Latency to Power OFF APU3 core\n"
-        time echo 0 > /sys/devices/system/cpu/cpu3/online
-        sync_apu_rpu
+    printf "\nAPU: Latency to Power OFF APU3 core\n"
+    time echo 0 > /sys/devices/system/cpu/cpu3/online
+    sync_apu_rpu
 fi
 
 echo "APU: ************ APU (APU0 only) full load, RPU idle, PL is OFF *********************"
@@ -155,12 +152,14 @@ sync_apu_rpu
 
 printf "APU: Setting APU0 frequency to ${low_freq}\n"
 time echo ${low_freq} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
+
 echo "APU: ********** APU (APU0 low freq) full load, RPU idle, PL is OFF *******************"
 sync_apu_rpu
 
 time killall yes
 printf "APU: Setting APU0 frequency to ${high_freq}\n"
 echo ${high_freq} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
+
 echo "APU: *********************** APU Linux Idle, RPU idle, PL is OFF *********************"
 sync_apu_rpu
 ggs_val=$(cat ${PGGS_INTERFACE})
@@ -170,41 +169,41 @@ echo $pggs_val > ${PGGS_INTERFACE}
 
 while :
 do
+    ggs_val=$(cat ${PGGS_INTERFACE})
+    ggs=$(( $ggs_val & 0xFF00 ))
+
+#    usleep 100
+
+    if [ $ggs == $((0xaa00)) ]; then
+        echo "APU: Sending sync command"
         ggs_val=$(cat ${PGGS_INTERFACE})
-        ggs=$(( $ggs_val & 0xFF00 ))
-
-#        usleep 100
-
-        if [ $ggs == $((0xaa00)) ]; then
-                echo "APU: Sending sync command"
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFF00FF))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-        elif [ $ggs == $((0xab00)) ]; then
-                echo "APU: APU Suspend request came from RPU"
-	        echo $pggs_val > ${PGGS_INTERFACE}
-	        ggs_val=$(cat ${PGGS_INTERFACE})
-        	pggs_val=$(($ggs_val & 0xFFFF00FF))
-                ggs_val=$(printf '%x\n' $pggs_val)
-                echo mem > /sys/power/state
-        elif [ $ggs == $((0x5500)) ]; then
-                echo "APU: Resumed script successfully"
-                echo "APU: Sending sync command"
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-                ggs_val=$(cat ${PGGS_INTERFACE})
-                pggs_val=$(($ggs_val & 0xFFFF00FF))
-                pggs_val=$(printf '%x\n' $pggs_val)
-                echo $pggs_val > ${PGGS_INTERFACE}
-                break
-        fi
+        pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
+        pggs_val=$(printf '%x\n' $pggs_val)
+        echo $pggs_val > ${PGGS_INTERFACE}
+        ggs_val=$(cat ${PGGS_INTERFACE})
+        pggs_val=$(($ggs_val & 0xFFFF00FF))
+        pggs_val=$(printf '%x\n' $pggs_val)
+        echo $pggs_val > ${PGGS_INTERFACE}
+    elif [ $ggs == $((0xab00)) ]; then
+        echo "APU: APU Suspend request came from RPU"
+        echo $pggs_val > ${PGGS_INTERFACE}
+        ggs_val=$(cat ${PGGS_INTERFACE})
+        pggs_val=$(($ggs_val & 0xFFFF00FF))
+        ggs_val=$(printf '%x\n' $pggs_val)
+        echo mem > /sys/power/state
+    elif [ $ggs == $((0x5500)) ]; then
+        echo "APU: Resumed script successfully"
+        echo "APU: Sending sync command"
+        ggs_val=$(cat ${PGGS_INTERFACE})
+        pggs_val=$(($ggs_val & 0xFFFFFF00 | 0xA5))
+        pggs_val=$(printf '%x\n' $pggs_val)
+        echo $pggs_val > ${PGGS_INTERFACE}
+        ggs_val=$(cat ${PGGS_INTERFACE})
+        pggs_val=$(($ggs_val & 0xFFFF00FF))
+        pggs_val=$(printf '%x\n' $pggs_val)
+        echo $pggs_val > ${PGGS_INTERFACE}
+        break
+    fi
 done
 
 
@@ -214,8 +213,8 @@ sync_apu_rpu
 yes > /dev/null &
 yes > /dev/null &
 if [ $Interface == "zynqmp" ]; then
-        yes > /dev/null &
-        yes > /dev/null &
+    yes > /dev/null &
+    yes > /dev/null &
 fi
 
 printf "APU: Setting APU0 frequency to ${low_freq}\n"
@@ -225,6 +224,7 @@ sync_apu_rpu
 
 printf "APU: Setting APU0 frequency to ${high_freq}\n"
 time echo ${high_freq} > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
+
 echo "APU: ************ APU (APU0 only) full load, RPU idle, PL is OFF *********************"
 sync_apu_rpu
 
@@ -233,13 +233,13 @@ time echo 1 > /sys/devices/system/cpu/cpu1/online
 sync_apu_rpu
 
 if [ $Interface == "zynqmp" ]; then
-        printf "\nAPU: Latency to Power ON APU2 core\n"
-        time echo 1 > /sys/devices/system/cpu/cpu2/online
-        sync_apu_rpu
+    printf "\nAPU: Latency to Power ON APU2 core\n"
+    time echo 1 > /sys/devices/system/cpu/cpu2/online
+    sync_apu_rpu
 
-        printf "\nAPU: Latency to Power ON APU3 core\n"
-        time echo 1 > /sys/devices/system/cpu/cpu3/online
-        sync_apu_rpu
+    printf "\nAPU: Latency to Power ON APU3 core\n"
+    time echo 1 > /sys/devices/system/cpu/cpu3/online
+    sync_apu_rpu
 fi
 
 echo "APU: ********************* APU full load, RPU idle, PL is OFF ************************"
@@ -254,17 +254,18 @@ printf "APU: Delay ${DelayVal} seconds\n\n"
 sleep ${DelayVal}
 
 if [ $Interface == "zynqmp" ]; then
-        echo "APU: Latency to Power ON PL domain"
-        ggs_val=$(cat ${PGGS_INTERFACE})
-        pggs_val=$(($ggs_val & 0xFFFFFF00 | 0x12))
-        pggs_val=$(printf '%x\n' $pggs_val)
-        echo $pggs_val > ${PGGS_INTERFACE}
+    echo "APU: Latency to Power ON PL domain"
+    ggs_val=$(cat ${PGGS_INTERFACE})
+    pggs_val=$(($ggs_val & 0xFFFFFF00 | 0x12))
+    pggs_val=$(printf '%x\n' $pggs_val)
+    echo $pggs_val > ${PGGS_INTERFACE}
 else
-        echo "APU: Powering on PL domain"
-        # load partial pdi
-        time fpgautil -R
-        time fpgautil -b partial.pdi
+    echo "APU: Powering on PL domain"
+    # load partial pdi
+    time fpgautil -R
+    time fpgautil -b /usr/bin/partial.pdi
 fi
+
 echo "APU: *********************** APU, RPU and PL in high power ***************************"
 printf "APU: Delay ${DelayVal} seconds\n\n"
 sleep ${DelayVal}
